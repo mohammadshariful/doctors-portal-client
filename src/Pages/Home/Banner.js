848ffec10521bd057bde8b3e9e6c74a0e1b47b3e
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-05-10 11:53:06 +0600

    treatment section added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,20 +1,19 @@
 import React from "react";
 import chair from "../../assets/images/chair.png";
+import PrimaryBtn from "./PrimaryBtn";
 const Banner = () => {
   return (
     <div className="hero min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img src={chair} className="max-w-sm rounded-lg shadow-2xl" alt="" />
-        <div>
+        <div className="w-100 lg:w-1/2 mx-auto">
           <h1 className="text-5xl font-bold">Your New Smile Starts Here!</h1>
           <p className="py-6">
             Lorem Ipsum is simply dummy text of the printing and typesetting
             industry. Lorem Ipsum has been the industry's standard dummy text
             ever since the.
           </p>
-          <button className="btn btn-secondary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">
-            Get Started
-          </button>
+          <PrimaryBtn />
         </div>
       </div>
     </div>
---
+++ src/Pages/Home/PrimaryBtn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PrimaryBtn = () => {
+  return (
+    <button className="btn btn-secondary uppercase text-white font-bold bg-gradient-to-r from-secondary to-primary">
+      Get Started
+    </button>
+  );
+};
+
+export default PrimaryBtn;
